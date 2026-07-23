1c053f4821844b016c198d2badd6d15cabf8108d
Cauan Mengali <cauanmengali10@gmail.com>
2026-07-23 18:57:06 -0300

    implementado controller e infraRepository

--- src/main/java/br/com/wakax/wakax_ecommerce/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/wakax/wakax_ecommerce/cliente/application/api/ClienteController.java
@@ -2,11 +2,16 @@ package br.com.wakax.wakax_ecommerce.cliente.application.api;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.wakax.wakax_ecommerce.cliente.application.api.request.ClienteRequest;
 import br.com.wakax.wakax_ecommerce.cliente.application.api.response.ClienteResponse;
+import br.com.wakax.wakax_ecommerce.cliente.application.api.response.ClienteResumoResponse;
+import br.com.wakax.wakax_ecommerce.cliente.application.api.response.PageResponse;
 import br.com.wakax.wakax_ecommerce.cliente.application.service.ClienteService;
+import br.com.wakax.wakax_ecommerce.cliente.domain.Cliente;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -31,5 +36,14 @@ public class ClienteController implements ClienteApi {
     ClienteResponse cliente = clienteService.buscaClienteEspecifico(idCliente);
     log.debug("[finish] ClienteController - buscaClienteEspecifico");
     return cliente;
+  }
+
+  @Override
+  public PageResponse<ClienteResumoResponse> buscarTodosClientes(int page, int size) {
+    log.debug("[start] ClienteController - buscarTodosClientes");
+    Page<Cliente> clientes = clienteService.buscarTodosClientes(PageRequest.of(page, size));
+    Page<ClienteResumoResponse> response = clientes.map(ClienteResumoResponse::new);
+    log.debug("[finish] ClienteController - buscarTodosClientes");
+    return PageResponse.from(response);
   }
 }
