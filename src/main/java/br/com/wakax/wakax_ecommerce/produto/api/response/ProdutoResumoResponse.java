0f6c4997f074bbda1b23e2eff2709ae93ef733b6
Ricardo Silva <pricardo.sribeiro@gmail.com>
2026-07-27 00:50:40 -0300

    feat: cria dto de resumo de produto para listagem

--- src/main/java/br/com/wakax/wakax_ecommerce/produto/api/response/ProdutoResumoResponse.java
+++ src/main/java/br/com/wakax/wakax_ecommerce/produto/api/response/ProdutoResumoResponse.java
@@ -1,4 +1,24 @@
 package br.com.wakax.wakax_ecommerce.produto.api.response;
 
+import br.com.wakax.wakax_ecommerce.produto.domain.Preco;
+import br.com.wakax.wakax_ecommerce.produto.domain.Produto;
+import br.com.wakax.wakax_ecommerce.produto.domain.StatusProduto;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Getter
 public class ProdutoResumoResponse {
+    private final String descricao;
+    private final StatusProduto status;
+    private final BigDecimal preco;
+    //private final Date dataCadastro;
+
+    public ProdutoResumoResponse(Produto produto) {
+        this.descricao = produto.getDescricao();
+        this.status = produto.getStatus();
+        this.preco = produto.getPrecoAtual();
+        //this.dataCadastro = dataCadastro;
+    }
 }
