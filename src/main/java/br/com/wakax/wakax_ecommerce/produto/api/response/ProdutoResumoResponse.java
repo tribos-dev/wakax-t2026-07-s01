df585efe61bb7d9b09f3d8323ef83cb244a08f1c
Ricardo Silva <pricardo.sribeiro@gmail.com>
2026-07-29 08:06:33 -0300

    refactor: padroniza contrato do ProdutoResumoResponse (precoAtual + formato de dataCadastro)

--- src/main/java/br/com/wakax/wakax_ecommerce/produto/api/response/ProdutoResumoResponse.java
+++ src/main/java/br/com/wakax/wakax_ecommerce/produto/api/response/ProdutoResumoResponse.java
@@ -5,20 +5,22 @@ import java.time.LocalDateTime;
 
 import br.com.wakax.wakax_ecommerce.produto.domain.Produto;
 import br.com.wakax.wakax_ecommerce.produto.domain.StatusProduto;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 
 @Getter
 public class ProdutoResumoResponse {
   private final String descricao;
   private final StatusProduto status;
-  private final BigDecimal preco;
-
+  private final BigDecimal precoAtual;
+  
+  @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
   private final LocalDateTime dataCadastro;
 
   public ProdutoResumoResponse(Produto produto) {
     this.descricao = produto.getDescricao();
     this.status = produto.getStatus();
-    this.preco = produto.getPrecoAtual();
+    this.precoAtual = produto.getPrecoAtual();
     this.dataCadastro = produto.getDataCriacao();
   }
 }
