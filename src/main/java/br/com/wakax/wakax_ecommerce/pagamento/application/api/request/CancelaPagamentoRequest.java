a0f76e7265a1c4f2e7164be35958c775d28bc42b
Cauan Mengali <cauanmengali10@gmail.com>
2026-08-24 19:03:09 -0300

    feat: cria request de cancelamento com motivo

---
+++ src/main/java/br/com/wakax/wakax_ecommerce/pagamento/application/api/request/CancelaPagamentoRequest.java
@@ -0,0 +1,14 @@
+package br.com.wakax.wakax_ecommerce.pagamento.application.api.request;
+
+
+import lombok.*;
+
+@Getter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CancelaPagamentoRequest {
+    private String motivo;
+}
+
