0909e453c3426d6d4290ce4438d3c21923049de4
Cauan Mengali <cauanmengali10@gmail.com>
2026-08-24 20:34:10 -0300

    fix: corrige request adicionando anotação

--- src/main/java/br/com/wakax/wakax_ecommerce/pagamento/application/api/request/CancelaPagamentoRequest.java
+++ src/main/java/br/com/wakax/wakax_ecommerce/pagamento/application/api/request/CancelaPagamentoRequest.java
@@ -1,5 +1,6 @@
 package br.com.wakax.wakax_ecommerce.pagamento.application.api.request;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
 @Getter
@@ -8,5 +9,6 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CancelaPagamentoRequest {
+  @JsonInclude(JsonInclude.Include.NON_EMPTY)
   private String motivo;
 }
