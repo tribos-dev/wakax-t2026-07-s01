748605d07e118d32e4e347bf175698518b0b6f5c
Cauan Mengali <cauanmengali10@gmail.com>
2026-08-30 20:50:23 -0300

    fix: adiciona @NotBlank no campo motivo para não persistir nulo no banco.

--- src/main/java/br/com/wakax/wakax_ecommerce/pagamento/application/api/request/CancelaPagamentoRequest.java
+++ src/main/java/br/com/wakax/wakax_ecommerce/pagamento/application/api/request/CancelaPagamentoRequest.java
@@ -2,11 +2,14 @@ package br.com.wakax.wakax_ecommerce.pagamento.application.api.request;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @ToString
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class CancelaPagamentoRequest {
+  @NotBlank(message = "{validacao.motivo.cancelamento}")
   private String motivo;
 }
