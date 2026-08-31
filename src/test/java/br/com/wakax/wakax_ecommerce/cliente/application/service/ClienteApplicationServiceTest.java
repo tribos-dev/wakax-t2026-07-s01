cac9a78fcfbaa5eb5be52c8f7d2dabb7644d5411
Henrique Nunes Nascimento <h3nriqu3.nun3s1@gmail.com>
2026-08-30 22:20:25 -0300

    (WX-17): Fix ClienteAppli...Test

--- src/test/java/br/com/wakax/wakax_ecommerce/cliente/application/service/ClienteApplicationServiceTest.java
+++ src/test/java/br/com/wakax/wakax_ecommerce/cliente/application/service/ClienteApplicationServiceTest.java
@@ -1,16 +1,11 @@
 package br.com.wakax.wakax_ecommerce.cliente.application.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 import java.util.List;
-import java.util.UUID;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -20,14 +15,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.test.util.ReflectionTestUtils;
 
 import br.com.wakax.wakax_ecommerce.cliente.application.api.request.ClienteRequest;
-import br.com.wakax.wakax_ecommerce.cliente.application.api.response.ClienteResponse;
 import br.com.wakax.wakax_ecommerce.cliente.application.repository.ClienteRepository;
 import br.com.wakax.wakax_ecommerce.cliente.domain.Cliente;
-import br.com.wakax.wakax_ecommerce.handler.APIException;
 
 @ExtendWith(MockitoExtension.class)
 public class ClienteApplicationServiceTest {
